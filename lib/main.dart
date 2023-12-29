7650ac7631b71b396641fe299ee251fbff0f3fa8
Ngarama97 <mbakary.mb@gmail.com>
2023-12-29 14:14:08 +0300

    Added recommended_food section, with customScrollView

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:delivery_app/pages/food/popular_food_detail.dart';
+import 'package:delivery_app/pages/food/recommended_food_detail.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const PopularFoodDetail(),
+      home: const RecommendedFoodDetail(),
     );
   }
 }
